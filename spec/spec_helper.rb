090126719581fc8194c650bba5cf0c617a43ce82
Gabriel <zhchuan.2016@gmail.com>
2023-09-01 13:28:49 +0800

    fix: codecov coverage doesn't check lib folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,13 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
+# Must be at top of file
+require "simplecov"
+require 'simplecov-cobertura'
+
+SimpleCov.start
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
+# Other
 require "bento-sdk"
 require 'faraday'
 require "pry"
-
-require "simplecov"
-SimpleCov.start
-
-require 'simplecov-cobertura'
-SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
